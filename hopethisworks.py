62df1082ee17dfff61d0c60bd473fad0bbca2e1f
Zhengji Li <127774804+xinjiav2@users.noreply.github.com>
2025-04-15 18:28:42 -0700

    fix ordering

--- hopethisworks.py
+++ hopethisworks.py
@@ -24,10 +24,11 @@ async def on_message(message):
 
     if "optix is bad" in content or "robotics is bad" in content:
         await message.add_reaction("👎")
-    elif "optix" in content or "robotics" in content:
-        await message.add_reaction("👎")
     elif "santhosh" in content and "optix" and "good" in content:
         await message.add_reaction('👍')
+    elif "optix" in content or "robotics" in content:
+        await message.add_reaction("👎")
+
 
 
 bot.run(TOKEN)
