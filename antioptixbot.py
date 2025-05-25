b889410f976776d49c7ecf818d51ddde08f879a3
Zhengjie Xu <nishiaaX2@gmail.com>
2025-05-25 11:26:30 -0700

    change the token..?

--- antioptixbot.py
+++ antioptixbot.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+TOKEN = os.getenv("ANTIOPTIXBOTTOKEN")
 
 intents = discord.Intents.default()
 intents.messages = True
